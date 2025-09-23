d44efac0a6de2ed070c293f8db6653d8b4aad416
Masum_Billah_Sifat <m.sifat069167@gmail.com>
2025-09-23 15:54:11 +0600

    create sitemap for better seo through package.json and next sitemap config file

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.ebdaaaltakwenco.com/', // ⬅️ replace with your actual domain
+  generateRobotsTxt: true,            // ✅ also generate robots.txt
+  sitemapSize: 5000,
+}
